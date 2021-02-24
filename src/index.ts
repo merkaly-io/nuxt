6432d59f34602c0dc4f29a1b066f30204610bdb7
Randy Tellez Galan <kronhyx@gmail.com>
2021-02-24 15:57:00 -0300

    perf: show package version instead environment package version

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,37 @@
 import { Module } from '@nuxt/types'
 import { ModuleThis } from '@nuxt/types/config/module'
 import chalk from 'chalk'
-import { resolve } from 'path'
+import packageJson from '../package.json'
+import pluginPath from './plugins/path'
 
-export interface NuxtPluginPath {
+export interface MerkalyNuxt {
   $path (name: String, params?: Record<string, any>): Object
 }
 
 declare module 'vue/types/vue' {
-  interface Vue extends NuxtPluginPath {
+  interface Vue extends MerkalyNuxt {
   }
 }
 
 declare module '@nuxt/types' {
-  interface NuxtAppOptions extends NuxtPluginPath {
+  interface NuxtAppOptions extends MerkalyNuxt {
   }
 
-  interface Context extends NuxtPluginPath {
+  interface Context extends MerkalyNuxt {
   }
 }
 
 declare module 'vuex/types/index' {
-  interface Store<S> extends NuxtPluginPath {
+  interface Store<S> extends MerkalyNuxt {
   }
 }
 
 const nuxtModule: Module<ModuleThis> = function () {
   const { nuxt, options } = this
-
-  this.addPlugin(resolve(__dirname, 'src/plugins/path.ts'))
+  this.addPlugin(pluginPath)
 
   nuxt.hook('listen', async () => {
-    options.cli.badgeMessages.push('Merklay: @v', chalk.underline.redBright(process.env.npm_package_version))
+    options.cli.badgeMessages.push(`Merklay: @v${chalk.underline.redBright(packageJson.version)}`)
   })
 
 }
