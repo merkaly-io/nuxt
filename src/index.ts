be13660e204dbff0c9958f869c2ce1d2bfbe01db
Randy Tellez Galan <kronhyx@gmail.com>
2021-02-24 15:08:34 -0300

    refactor: split definitions and source files

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,28 @@ import { ModuleThis } from '@nuxt/types/config/module'
 import chalk from 'chalk'
 import { resolve } from 'path'
 
+export interface NuxtPluginPath {
+  $path (name: String, params?: Record<string, any>): Object
+}
+
+declare module 'vue/types/vue' {
+  interface Vue extends NuxtPluginPath {
+  }
+}
+
+declare module '@nuxt/types' {
+  interface NuxtAppOptions extends NuxtPluginPath {
+  }
+
+  interface Context extends NuxtPluginPath {
+  }
+}
+
+declare module 'vuex/types/index' {
+  interface Store<S> extends NuxtPluginPath {
+  }
+}
+
 const nuxtModule: Module<ModuleThis> = function () {
   const { nuxt, options } = this
 
