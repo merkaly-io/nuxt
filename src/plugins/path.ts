73cfd42197903b3f321d55d89c53dd9f56eb4eb1
Randy Tellez Galan <kronhyx@gmail.com>
2021-02-24 21:22:42 -0300

    fix: wrong main entry point directory

--- src/plugins/path.ts
+++ src/plugins/path.ts
@@ -1,6 +1,7 @@
 import { Context } from '@nuxt/types'
+import { Inject } from '@nuxt/types/app'
 
-export default ({ app }: Context, inject: Function): Function => {
+export default ({ app }: Context, inject: Inject): Function => {
   app.$path = (name: String, params: Object = {}): Object => ({ name, params })
   inject('path', app.$path)
 
