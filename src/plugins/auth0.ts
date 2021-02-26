d18a7690e540ea01622fccdb1b2f69fbb5fdaf8c
Randy Tellez Galan <kronhyx@gmail.com>
2021-02-25 21:41:03 -0300

    perf: add responseType to auth plugin

--- src/plugins/auth0.ts
+++ src/plugins/auth0.ts
@@ -4,7 +4,8 @@ import { AuthOptions, WebAuth } from 'auth0-js'
 export default ({ app, $config }: Context, inject: Function) => {
   const options: AuthOptions = {
     domain: String($config.auth0.AUTH0_DOMAIN),
-    clientID: String($config.auth0.AUTH0_CLIENT)
+    clientID: String($config.auth0.AUTH0_CLIENT),
+    responseType: 'token',
   }
 
   app.$auth0 = new WebAuth(options)
