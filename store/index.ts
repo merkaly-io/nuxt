5d0d5c32bb749c8e941eda75988c63f16e7e6b62
Randy Tellez Galán <kronhyx@gmail.com>
2021-07-20 12:39:48 -0300

    chore: add local vuexStore

---
+++ store/index.ts
@@ -0,0 +1,13 @@
+import { Auth } from '@nuxtjs/auth-next'
+
+export default interface IndeX {
+  auth: Auth
+}
+
+export const state = (): Partial<IndeX> => ({})
+
+export const mutations: any = {}
+
+export const actions: any = {}
+
+export const getters = {}
