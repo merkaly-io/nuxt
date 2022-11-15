f2bdf0621701dfcfa0d81c02236385bc42390a5c
Randy Tellez Galan <kronhyx@gmail.com>
2022-11-15 12:14:10 -0300

    refactor: add baseURL axios env var

--- nuxt.ts
+++ nuxt.ts
@@ -1,64 +1,77 @@
 import { NuxtConfig } from '@nuxt/types/config'
-import merge from 'lodash.merge'
+// eslint-disable-next-line import/named
+import { merge } from 'lodash'
 
-export const defineConfig = (config: NuxtConfig): NuxtConfig => merge({
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
-    ]
-  },
+export const defineConfig = (extendedConfig: NuxtConfig): NuxtConfig => {
+  const mainConfig: NuxtConfig = ({
+    // Global page headers: https://go.nuxtjs.dev/config-head
+    head: {
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: '' },
+        { name: 'format-detection', content: 'telephone=no' }
+      ]
+    },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+    publicRuntimeConfig: {
+      baseUrl: String(process.env.BASE_URL)
+    },
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: [],
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/typescript
-    ['@nuxt/typescript-build', {}],
-    // https://go.nuxtjs.dev/stylelint
-    ['@nuxtjs/stylelint-module', {}]
-  ],
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://axios.nuxtjs.org/
-    ['@nuxtjs/axios', {}],
-    // https://pwa.nuxtjs.org/
-    ['@nuxtjs/pwa', {}],
-    // https://content.nuxtjs.org/
-    ['@nuxt/content', {}],
-    // https://sentry.nuxtjs.org/
-    ['@nuxtjs/sentry', { disabled: Boolean(!process.env.SENTRY_DSN) }]
-  ],
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [
+      // https://go.nuxtjs.dev/typescript
+      ['@nuxt/typescript-build', {}],
+      // https://go.nuxtjs.dev/stylelint
+      ['@nuxtjs/stylelint-module', {}]
+    ],
 
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/'
-  },
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [
+      // https://axios.nuxtjs.org/
+      ['@nuxtjs/axios', {}],
+      // https://pwa.nuxtjs.org/
+      ['@nuxtjs/pwa', {}],
+      // https://content.nuxtjs.org/
+      ['@nuxt/content', {}],
+      // https://sentry.nuxtjs.org/
+      ['@nuxtjs/sentry', { disabled: Boolean(!process.env.SENTRY_DSN) }],
+      // https://github.com/nuxt-community/moment-module/
+      ['@nuxtjs/moment', {}],
+      // https://auth.nuxtjs.org/
+      ['@nuxtjs/auth-next', {}]
+    ],
 
-  // PWA module configuration: https://go.nuxtjs.dev/pwa
-  pwa: {
-    manifest: {
-      lang: 'en'
+    // Axios module configuration: https://go.nuxtjs.dev/config-axios
+    axios: {
+      // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+      baseURL: String(process.env.BASE_URL)
+    },
+
+    // PWA module configuration: https://go.nuxtjs.dev/pwa
+    pwa: {
+      manifest: {
+        lang: 'en'
+      }
+    },
+
+    // Content module configuration: https://go.nuxtjs.dev/config-content
+    content: {},
+
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {
+      standalone: true,
+      babel: {
+        compact: true
+      }
     }
-  },
+  })
 
-  // Content module configuration: https://go.nuxtjs.dev/config-content
-  content: {},
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-    standalone: true,
-    babel: {
-      compact: true
-    }
-  }
-}, config)
+  return merge<NuxtConfig, NuxtConfig>(mainConfig, extendedConfig)
+}
